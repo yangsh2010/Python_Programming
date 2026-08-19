3c05ff469d29578069872fa69d2a46ff9fd2439a
yangsh2010 <didtngur0105@gmail.com>
2026-08-19 10:32:33 +0900

    Update input example

--- 00_io/ex1.py
+++ 00_io/ex1.py
@@ -3,3 +3,33 @@
 # 1개 입력
 a = input()
 print(a)
+print(type(a))
+
+# 정수 변환
+a = input()
+a = int (a)
+print(type(a))
+
+# 한번에 쓰자
+a = int(input())
+print(type(a))
+
+# 실수 입력
+b = float(input())
+print(b, type(b))
+
+# 정수 2개 입력
+# 100
+# 200
+a = int(input())
+b = int(input())
+print(a, b)
+
+# 100 200 입력
+input().split()
+print(a)
+
+# map 사용하기
+# map(함수, 리스트)
+a, b, c = map(int, input().split())
+print(a, b, c)
